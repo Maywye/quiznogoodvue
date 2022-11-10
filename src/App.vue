a3668429d154e3f41c9cf3fffcab72034a141ce4
Marine <marine.jacobs@live.com>
2022-11-10 16:53:07 +0100

    add components footer header

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
   <div id="app">
-    <h1>QuizNoGood</h1>
+    <div class="flex flex-col h-screen bg-[#502F4C]">
+      <TheHeader />
+      <TheFooter />
+    </div>
   </div>
 </template>
 
 <script>
-
+import TheHeader from './components/HeaderVue.vue';
+import TheFooter from './components/FooterVue.vue';
 export default {
   name: 'App',
   components: {
+    TheHeader,
+    TheFooter
   }
 }
 </script>
---
+++ src/components/FooterVue.vue
@@ -0,0 +1,24 @@
+<template>
+        <footer class="bottom-0 w-full bg-[#F9F4F5] border-gray-200 py-2.5" >
+        <div class=" flex flex-wrap justify-between ml-10 mr-10 grid grid-cols-3">
+            <div></div>
+            <p class="block text-center content-center">        Copyright - QuizNoGood {{date}}        </p>
+            <a href="#" class="block text-end italic underline hover:text-[#C8B8DB]">Ne pas nous contacter</a>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: "TheFooter.vue",
+    data(){
+        return {
+            date: new Date().getFullYear()
+        }
+    }
+    
+}
+</script>
+
+<style> 
+</style>
