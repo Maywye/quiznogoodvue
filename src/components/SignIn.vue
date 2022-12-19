f79940b3091cbc91018e581246921925f1b60eb5
Leith <112956932+bambstk@users.noreply.github.com>
2022-12-19 11:32:38 +0100

    id differents

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -3,11 +3,11 @@
             <h2 class="text-center p-3 md:text-5xl">Inscription</h2>
             <form class="grid grid-cols-1 place-content-around md:my-12 md:mx-24">
                 <label for="Pseudo" class="ml-4 md:text-lg text-[#502F4C] font-medium">Pseudo <span class="text-sm hover:text-base">( Choisi bien, tu ne pourras pas le modifier )</span></label>
-                <input type="text" name="Pseudo" id="pseudo" class="rounded-lg px-2 py-3 bg-[#C8B8DB] outline-[#502F4C] w-full mb-5">
+                <input type="text" name="Pseudo" id="pseudoSignIn" class="rounded-lg px-2 py-3 bg-[#C8B8DB] outline-[#502F4C] w-full mb-5">
                 <label for="mail" class="ml-4 text-lg text-[#502F4C] font-medium">Adresse Email</label>
                 <input type="email" name="mail" id="mail" class="rounded-lg px-2 py-3 bg-[#C8B8DB] outline-[#502F4C] w-full mb-5">
                 <label for="mdp" class="ml-4 text-lg text-[#502F4C] font-medium">Mot de passe</label>
-                <input type="password" name="mdp" id="mdp" class="rounded-lg px-2 py-3 bg-[#C8B8DB] outline-[#502F4C] w-full mb-5">
+                <input type="password" name="mdp" id="mdpSignIn" class="rounded-lg px-2 py-3 bg-[#C8B8DB] outline-[#502F4C] w-full mb-5">
                 <label for="cmdp" class="ml-4 text-lg text-[#502F4C] font-medium">Confirmation du mot de passe</label>
                 <input type="password" name="cmdp" id="cmdp" class="rounded-lg px-2 py-3 bg-[#C8B8DB] outline-[#502F4C] w-full mb-5">
                 <br>
